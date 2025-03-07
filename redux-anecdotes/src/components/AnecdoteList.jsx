a524994e17212bd1ff4d69590cbbddba5d1dfef2
ElisaSonera <elisa.laatu@gmail.com>
2025-03-07 11:32:17 +0200

    6.13

--- redux-anecdotes/src/components/AnecdoteList.jsx
+++ redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,26 +1,30 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
+import { changeNotification } from '../reducers/notificationReducer'
 
 // eslint-disable-next-line react/prop-types
 const Anecdote = ({ content, handleClick, votes }) => {
   return (
     <div>
       {content}
-      <div>has {votes} votes <button onClick={handleClick}>vote</button></div>
+      <div>
+        has {votes} votes <button onClick={handleClick}>vote</button>
+      </div>
     </div>
   )
 }
 
 const Anecdotes = () => {
   const dispatch = useDispatch()
-  const filter = useSelector(state => state.filter)
+  const filter = useSelector((state) => state.filter)
 
-  const filterAnecdotes = useSelector(state => {
+  const filterAnecdotes = useSelector((state) => {
     if (state.filter === 'ALL') {
       return state.anecdotes
     }
-    return state.anecdotes.filter(anecdote =>
-      anecdote.content.includes(filter))
+    return state.anecdotes.filter((anecdote) =>
+      anecdote.content.includes(filter)
+    )
   })
 
   return (
@@ -29,7 +33,13 @@ const Anecdotes = () => {
         <Anecdote
           key={anecdote.id}
           content={anecdote.content}
-          handleClick={() => dispatch(vote(anecdote.id))}
+          handleClick={() => {
+            dispatch(vote(anecdote.id))
+            dispatch(changeNotification(`you voted '${anecdote.content}'`))
+            setTimeout(() => {
+              dispatch(changeNotification(''))
+            }, 5000)
+          }}
           votes={anecdote.votes}
         />
       ))}
--- redux-anecdotes/src/reducers/notificationReducer.js
+++ redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,9 +1,24 @@
 //6.12
 //createSlice
-//riittää että sovellus toimii ja näyttää 
+//riittää että sovellus toimii ja näyttää
 // notificationReducerin alkuarvoksi asettaman viestin.
 
 // 6.13
-// näyttää Notification-komponentin avulla viiden sekunnin ajan, kun 
+// näyttää Notification-komponentin avulla viiden sekunnin ajan, kun
 // sovelluksessa äänestetään tai luodaan uusia anekdootteja
 // Notifikaation asettamista ja poistamista varten kannattaa toteuttaa action creatorit.
+
+import { createSlice } from '@reduxjs/toolkit'
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState: "",
+  reducers: {
+    changeNotification(state, action) {
+      return action.payload
+    }
+  }
+})
+
+export const { changeNotification } = notificationSlice.actions
+export default notificationSlice.reducer
